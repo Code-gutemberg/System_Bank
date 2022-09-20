9cf5e5d2ddaca2ae9422b97e1b92e095e75f1487
Alex Gutemberg <gutembergup@gmail.com>
2022-09-20 15:46:32 -0300

    update 20-09-22

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from query.bank import Bank
+from query.account import Accounts
 from time import sleep
 import app.interface as interface
 import app.treatment as treatment
@@ -14,7 +15,7 @@ while True:
     sleep(2)
     if bank.auth(agency, account, password):
         name = bank.auth(agency, account, password)[1]
-        print('[SUCESS] - Usuário autenticado Logado')
+        print('[SUCESS] - Usuário autenticado')
         interface.title('INICIANDO SISTEMA')
         sleep(2)
         os.system('cls')
@@ -30,7 +31,8 @@ while True:
             elif menu == 2:
                 ...
             elif menu == 3:
-                ...
+                account01 = Accounts(agency, account, 0)
+                account01.account()
             elif menu == 4:
                 interface.title('[!] PROGRAMA ENCERRADO COM SUCESSO!')
                 sleep(2)
@@ -49,6 +51,16 @@ while True:
     break
 '''
 
+# Instanciando os clientes
+cliente1 = Cliente('Alex', 30)
+cliente2 = Cliente('Delis', 25)
+cliente3 = Cliente('Fabricio', 45)
+
+# Instanciando as contas
+conta1 = ContaCorrente('12345', '0001-01', 0)
+conta2 = ContaPoupanca('56789', '0002-02', 0)
+conta3 = ContaCorrente('09888', '03102-02', 0)
+
 # Instanciando as contas aos clientes
 cliente1.inserir_conta(conta1)
 cliente2.inserir_conta(conta2)
@@ -57,5 +69,6 @@ cliente3.inserir_conta(conta3)
 # Inserindo o cliente1 e a conta1 no banco
 banco.inserir_cliente(cliente1)
 banco.inserir_conta(conta1)
+
 
 '''
